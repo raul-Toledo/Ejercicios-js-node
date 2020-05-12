4d4f48df8d86c433e2579225ea378b0df6c16b54
Raul Toledo <ratp82@gmail.com>
2020-05-12 12:37:50 -0600

    Se modifico el archivo para ingresar los comentarios

--- Ciclos/gpo_paterno_nombre_3-3.js
+++ Ciclos/gpo_paterno_nombre_3-3.js
@@ -1,7 +1,7 @@
-//Nombre:
-//Grupo:
-//Grado:
-//Fecha:
+//Nombre: Raul Toledo
+//Grupo: 345trtg
+//Grado: 4
+//Fecha: 12 / mayo / 2020
 //Problema 3.3	Caja registradora
 let read = require('readline-sync');
 
