a340d18a2f9fb51516f06a47a056432c5ec91d1d
Raul Toledo <ratp82@gmail.com>
2020-05-12 10:27:36 -0600

    corrección de la redacción del problema

--- Ciclos/gpo_paterno_nombre_3-2.js
+++ Ciclos/gpo_paterno_nombre_3-2.js
@@ -2,6 +2,6 @@
 //Grupo:
 //Grado:
 //Fecha:
-//Problema 3.2 determinar, de N cantidades, cuántas son cero, cuántas son menores a cero, y cuántas son mayores a cero
+//Problema 3.2 Cuántas son cero cuántas son menores a cero y cuántas son mayores a cero
 let read = require('readline-sync');
 
