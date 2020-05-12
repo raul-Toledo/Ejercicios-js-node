eb867dea4855c0e7b12af99a0ae1ce9f6a640650
Raul Toledo <ratp82@gmail.com>
2020-05-12 10:30:24 -0600

    nombre de archvo

--- Ciclos/gpo_paterno_nombre_3-5.js
+++
@@ -1,7 +0,0 @@
-//Nombre:
-//Grupo:
-//Grado:
-//Fecha:
-//Problema 3.5.	tabla de multiplicar de un entero K
-let read = require('readline-sync');
-
---
+++ Ciclos/z_paterno_nombre_3-5.js
@@ -0,0 +1,7 @@
+//Nombre:
+//Grupo:
+//Grado:
+//Fecha:
+//Problema 3.5.	tabla de multiplicar de un entero K
+let read = require('readline-sync');
+
